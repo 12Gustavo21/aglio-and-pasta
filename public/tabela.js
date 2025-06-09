246bbbddc14e41d892d4df23f2b614d0d471d038
Miguel Souza <miguelmdr0410@gmail.com>
2025-06-09 17:18:54 -0300

    tabela

---
+++ public/tabela.js
@@ -0,0 +1,86 @@
+const itemsData = [
+    {
+      imagePath: "./assets/images/coca-cola.jpg",
+      name: "Coca-Cola Gelada",
+      description: "Refrigerante clássico bem gelado, perfeito para acompanhar qualquer refeição",
+      category: "bebidas",
+      rating: 8.5
+    },
+    {
+      imagePath: "./assets/images/brigadeiro.jpg",
+      name: "Brigadeiro Gourmet",
+      description: "Doce brasileiro tradicional feito com chocolate belga e granulado especial",
+      category: "doces",
+      rating: 9.2
+    },
+    {
+      imagePath: "./assets/images/coxinha.jpg",
+      name: "Coxinha de Frango",
+      description: "Salgado crocante recheado com frango desfiado temperado e catupiry",
+      category: "salgados",
+      rating: 8.8
+    },
+    {
+      imagePath: "./assets/images/pizza-margherita.jpg",
+      name: "Pizza Margherita",
+      description: "Pizza clássica com molho de tomate, mussarela, manjericão fresco e azeite",
+      category: "pizzas",
+      rating: 9.0
+    },
+    {
+      imagePath: "./assets/images/lasanha.jpg",
+      name: "Lasanha à Bolonhesa",
+      description: "Massa intercalada com molho bolonhesa, bechamel e queijo gratinado",
+      category: "massas",
+      rating: 9.3
+    },
+    {
+      imagePath: "./assets/images/suco-laranja.jpg",
+      name: "Suco de Laranja Natural",
+      description: "Suco fresco espremido na hora, rico em vitamina C e sem conservantes",
+      category: "bebidas",
+      rating: 8.0
+    },
+    {
+      imagePath: "./assets/images/pudim.jpg",
+      name: "Pudim de Leite Condensado",
+      description: "Sobremesa cremosa com calda de caramelo, feita com leite condensado",
+      category: "doces",
+      rating: 8.7
+    },
+    {
+      imagePath: "./assets/images/pastel.jpg",
+      name: "Pastel de Queijo",
+      description: "Pastel frito na hora com recheio cremoso de queijo derretido",
+      category: "salgados",
+      rating: 8.2
+    },
+    {
+      imagePath: "./assets/images/pizza-pepperoni.jpg",
+      name: "Pizza Pepperoni",
+      description: "Pizza com molho de tomate, mussarela e fatias generosas de pepperoni",
+      category: "pizzas",
+      rating: 8.9
+    },
+    {
+      imagePath: "./assets/images/espaguete.jpg",
+      name: "Espaguete Carbonara",
+      description: "Massa al dente com molho cremoso, bacon, ovos e queijo parmesão",
+      category: "massas",
+      rating: 9.1
+    },
+    {
+      imagePath: "./assets/images/cerveja.jpg",
+      name: "Cerveja Artesanal IPA",
+      description: "Cerveja artesanal com lúpulo americano, sabor intenso e aroma cítrico",
+      category: "bebidas",
+      rating: 8.4
+    },
+    {
+      imagePath: "./assets/images/brownie.jpg",
+      name: "Brownie de Chocolate",
+      description: "Brownie denso e úmido com pedaços de chocolate e nozes",
+      category: "doces",
+      rating: 8.9
+    }
+  ];
