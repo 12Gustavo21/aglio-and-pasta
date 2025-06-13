9bd8cd1d9d181d65662d3f0b1d19831d58d3f63e
12Gustavo21 <gustavo927318@gmail.com>
2025-06-13 16:00:43 -0300

    style: fix categories path

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "./src/categories.html"),
+        nested: resolve(__dirname, "./categories.html"),
       },
     },
   },
