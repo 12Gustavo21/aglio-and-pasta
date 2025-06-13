a72f1c56494a772cf77b1fe29cd591f63a584373
12Gustavo21 <gustavo927318@gmail.com>
2025-06-13 15:54:13 -0300

    build: fix rollup options

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "./src/categories.html"),
+      },
+    },
+  },
 });
