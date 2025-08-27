846596df4e87047d84e8abcacf4c2c45d74c3d8e
Arthur <almeida.lacerda@academico.ifpb.edu.br>
2025-08-27 15:04:54 -0300

    fix: delete aglio-and-pasta-develop directory

--- aglio-and-pasta-develop/src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const SUPABASE_URL = "https://zuwuwwfxmbomhuluhxim.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1d3V3d2Z4bWJvbWh1bHVoeGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2Nzc3MDMsImV4cCI6MjA3MDI1MzcwM30.bR94CwwYWtyG3uUAlUtfSsRvbn46SnK7fzy_sIpv90k";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
